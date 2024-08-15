8470d7b73fd2c6c1ec6e374dfae718d54defb2a3
Hezeria05 <hannahgracevsantos@iskolarngbayan.pup.edu.ph>
2024-08-15 17:31:15 +0800

    feat: login page (front end), configured the urls, views, and templates

--- CapstoneProject/FireDetection/views.py
+++ CapstoneProject/FireDetection/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+# For class-based views [CBV]
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+# Import the User class (model)
+from django.contrib.auth.models import User
+# Import the User class (model)
+
+
+def login_view(request):
+   return render(request, "login.html")
 
 # Create your views here.
-def home(request):
+def home_view(request):
     return render(request, "home.html")
