cd7bf8a7d8e82596bf62287ef75c9144d34662d5
Ronilo Rejuso <ronilorejuso21@gmail.com>
2024-09-02 21:00:28 +0800

    imports login view

--- CapstoneProject/FireDetection/urls.py
+++ CapstoneProject/FireDetection/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from .views import serve_css
+from .views import login_view
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
     path('login/', views.login_view, name='login'),
+    path('', views.home_view, name='home'),
+    
     path('analytics/', views.analytics_view, name='analytics'),
     path('test-css/', serve_css, name='test-css'),
 ]
