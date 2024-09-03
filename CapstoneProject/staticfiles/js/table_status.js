7682ae1e17108a0d03692b324388960ee68750dd
Hezeria05 <hannahgracevsantos@iskolarngbayan.pup.edu.ph>
2024-09-03 16:55:24 +0800

    feat: added a javascript function to categorize the styles of the status in accounts table

--- CapstoneProject/staticfiles/js/table_status.js
+++ CapstoneProject/staticfiles/js/table_status.js
@@ -11,3 +11,18 @@ document.querySelectorAll('.rb-table td:last-child h6').forEach((element) => {
         row.classList.add('status-resolved');
     }
 });
+
+document.querySelectorAll('.ab-table td:last-child h6').forEach((element) => {
+    const statusText = element.textContent.trim().toLowerCase();
+    const row = element.closest('tr'); // Get the parent row
+
+    // Apply border and background color based on status
+    if (statusText === 'unverified') {
+        element.style.border = '3px solid #ED7027';
+        row.classList.add('status-unverified');
+    } else if (statusText === 'verified') {
+        element.style.backgroundColor = '#ED7027';
+        row.classList.add('status-verified');
+    }
+});
+
