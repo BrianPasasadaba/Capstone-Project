34eb4abdec17b7e35a36add62574c4897507370c
Hezeria05 <hannahgracevsantos@iskolarngbayan.pup.edu.ph>
2024-09-06 21:13:58 +0800

    feat: include toast on the sidebar.html with javascript function

---
+++ CapstoneProject/staticfiles/js/toast.js
@@ -0,0 +1,11 @@
+const myToast = new bootstrap.Toast('.toast');
+
+setTimeout(() => {
+    myToast.show();
+}, 1000);
+
+// const myToast = new bootstrap.Toast('.toast', {
+//     autohide: false
+// });
+
+// myToast.show();
