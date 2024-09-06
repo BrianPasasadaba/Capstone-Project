356842a79d32404fdc684cc772061a54669c6a57
Hezeria05 <hannahgracevsantos@iskolarngbayan.pup.edu.ph>
2024-09-06 15:32:34 +0800

    refactor: separated the modals on a different html file

--- CapstoneProject/CapstoneProject/settings.py
+++ CapstoneProject/CapstoneProject/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Bfp',
-        'USER': 'Bfp',
+        'USER': 'postgres',
         'PASSWORD': 'Bfp',
         'HOST': 'localhost',
         'PORT': '5432',
--- CapstoneProject/staticfiles/js/form_controls.js
+++ CapstoneProject/staticfiles/js/form_controls.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('#create-report-form input, #create-report-form select').forEach(function(el) {
+document.querySelectorAll('#report-preview-form input, #report-preview-form select').forEach(function(el) {
     el.setAttribute('disabled', 'true');
     el.setAttribute('readonly', 'true');
 });
