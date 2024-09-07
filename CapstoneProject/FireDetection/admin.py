6e993ff0c02a891088249145fe09d1182803152f
Ronilo Rejuso <ronilorejuso21@gmail.com>
2024-09-08 04:59:04 +0800

    feat: added initial report in admin

--- CapstoneProject/FireDetection/admin.py
+++ CapstoneProject/FireDetection/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, InitialReport
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -22,12 +22,15 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
+class InitialReportAdmin(admin.ModelAdmin):
+    list_display = (
+        'where', 'date', 'time', 'time_of_fire_out', 'occupancy_type',
+        'name_of_owner', 'alarm_status', 'no_of_respondents', 'estimated_damage',
+        'no_of_establishments', 'no_of_casualties', 'no_of_injured', 'proof'
+    )
+    search_fields = ('where', 'occupancy_type', 'name_of_owner', 'alarm_status')
+    list_filter = ('date', 'time_of_fire_out', 'alarm_status')
+    readonly_fields = ('proof',)
+
 admin.site.register(CustomUser, CustomUserAdmin)
-
-from django import forms
-from .models import CustomUser
-
-class RegisterForms(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'first_name', 'last_name', 'date_of_birth')
+admin.site.register(InitialReport, InitialReportAdmin)
