24dd4d149b13d02da9b704bd1102dc18aa045749
Ronilo Rejuso <ronilorejuso21@gmail.com>
2024-10-20 23:44:49 +0800

    feat: added the super admin email account

--- CapstoneProject/CapstoneProject/settings.py
+++ CapstoneProject/CapstoneProject/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rejusocute101@gmail.com'
-EMAIL_HOST_PASSWORD = 'ecae dvfi mhpi aozw'  
+EMAIL_HOST_USER = 'bfpsuperad@gmail.com'
+EMAIL_HOST_PASSWORD = 'spyb tgrh kfht jttu'  
 DEFAULT_FROM_EMAIL = 'ronilorejuso21@gmail.com'
 
 
