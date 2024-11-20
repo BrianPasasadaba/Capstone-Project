421b140d3ace036cc87f16fd0abda4c5f8b2ab6e
BrianPasasadaba <briancyberrpasasadaba@iskolarngbayan.pup.edu.ph>
2024-11-20 22:48:16 +0800

    chore: update ALLOWED_HOSTS in settings.py

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -17,7 +17,10 @@ SECRET_KEY = 'django-insecure-3ie-9bx+39e+thj%2v+4k54y72y_4+-y4v+4r7n@t&e7gey9^g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 LOGIN_URL = '/'
 
